7b236b4c0aea4609265989e831fa629f384a9aec
Captain17 <thoailt.cnthongtin@gmail,com>
2021-11-26 11:33:13 +0700

    thoi

---
+++ Scripts/Login-true-user-pass-Giangvien/Script1637900094584.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('https://online.hcmue.edu.vn/')
+
+WebUI.click(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/a_ng nhp'))
+
+WebUI.click(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_Sinh vin_ctl00ContentPlaceHolder1ctl00ctl00Role'))
+
+WebUI.setText(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_Tn ng nhp_ctl00ContentPlaceHolder1ctl00ctl00txtUserName'), 
+    '4501104227')
+
+WebUI.setText(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_Mt m_ctl00ContentPlaceHolder1ctl00ctl00txtPassword'), 
+    'Justgivefat1#')
+
+WebUI.click(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_S lng truy cp_ctl00ContentPlaceHolder1ctl00ctl00btLogin'))
+
+WebUI.closeBrowser()
+
